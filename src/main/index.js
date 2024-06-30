6bf6800817f38884395c9cdd0f431668aeb7a76a
manistra <lor3nzo95@gmail.com>
2024-06-30 19:56:18 +0200

    added electron forge

--- src/main/index.js
+++ src/main/index.js
@@ -89,3 +89,4 @@ app.on('window-all-closed', () => {
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+if (require('electron-squirrel-startup') === true) app.quit()
